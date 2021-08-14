0aa26d6c1cea435f96b013a419b42fc3ab941e3c
Isaque Akamine <isaque.akamine@condomob.net>
2021-08-14 20:48:36 -0300

    removed package lock

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 12,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        semi:['error', 'always'],
-        quotes: ['error', 'single'],
-        indent: ['error', 4],
-        'comma-spacing': ['error', { before: false, after: true }]
-    }
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    semi:['error', 'always'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'comma-spacing': ['error', { before: false, after: true }]
+  }
 };
